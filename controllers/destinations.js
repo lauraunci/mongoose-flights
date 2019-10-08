6f2e69fee690411131b30f7f92805c9291627e8e
Laura Sora <laurasora@Lauras-MacBook-Pro.local>
2019-10-07 23:16:32 -0400

    final commit

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -2,10 +2,6 @@ const Flight = require('../models/flight');
 
 const create = (req, res) => {
     Flight.findById(req.params.id, function(err, flight) {
-        // if (!flight) {
-        //     return res.redirect('/flights');
-        // }
-
         flight.destinations.push(req.body);
         flight.save(function(err) {
             res.redirect(`/flights/${flight._id}`);
